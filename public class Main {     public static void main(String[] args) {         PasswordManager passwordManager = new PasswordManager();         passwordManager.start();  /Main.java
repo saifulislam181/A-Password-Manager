199bd3815e7dc9715078a216e6e010f531d0b88a
Md.Saiful Islam <83628269+saifulislam181@users.noreply.github.com>
2024-11-06 00:33:14 +0000

    Delete public class Main {     public static void main(String[] args) {         PasswordManager passwordManager = new PasswordManager();         passwordManager.start();   directory

--- public class Main {     public static void main(String[] args) {         PasswordManager passwordManager = new PasswordManager();         passwordManager.start();  /Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        PasswordManager passwordManager = new PasswordManager();
-        passwordManager.start();  // Start the console-based interface
-    }
-}
